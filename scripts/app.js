f18489cd6625c4e104054e328ea7af3cd1b54d6c
Atılhan Dursunoğlu <atilhandursunoglu@outlook.com>
2016-10-25 23:34:30 +0300

    primAlgoritm implemented.

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+sigma.classes.graph.addMethod("primsAlgorithm", primsAlgorithm);
+
 s = new sigma({
   graph: {
       nodes: [],
@@ -28,3 +30,45 @@ while(nodes.size() < 100)
 
 s.graph.read({nodes: nodes.values()});
 s.refresh();
+
+var nodeArray = nodes.values();
+var edges = new Array();
+var nodeEdgeMap = new buckets.Dictionary(function(object)
+{
+  return object.a > object.b ? object.b + " " + object.a : object.a + " " + object.b; 
+});
+for(i = 0; i < 99; i++)
+{
+  for(j = i + 1; j < 100; j++)
+  {
+    var lenX = nodeArray[i].x - nodeArray[j].x;
+    var lenY = nodeArray[i].y - nodeArray[j].y;
+    var weight = Math.sqrt(lenY * lenY + lenX * lenX);
+    nodeEdgeMap.set({a: i, b: j}, edges.length);
+    edges.push(new Edge(edges.length, i, j, weight));
+  }
+}
+
+
+var locations = new Array();
+s.bind('clickNode', function(e)
+{
+  switch(locations.length)
+  {
+    case 0:
+    {
+      locations.push(e.data.node);
+      e.data.node.color = "#4286f4";
+      s.refresh()
+    }break;
+    case 1:
+    {
+      s.graph.read({edges: edges});
+      locations.push(e.data.node);
+      e.data.node.color = "#59f442";
+
+      s.graph.primsAlgorithm(e.data.node.id, nodeEdgeMap);
+      s.refresh();
+    }break;
+  }
+}); 
--- scripts/libs/graphstructures.js
+++ scripts/libs/graphstructures.js
@@ -27,14 +27,81 @@ class Node
 
 class Edge
 {
-    constructor(id, label, source, target, weight)
+    constructor(id, source, target, weight)
     {
         this.id = id;
-        this.label = label;
+        this.label = Math.round(weight).toString();
         this.source = source;
         this.target = target;
         this.weight = weight;
     }
+}
+
+function KruskalAlgorithm(sortedEdges)
+{
+    var newEdges = new Array();
+    var nodes = new Array();
+
+    for(i = 0; i < sortedEdges.length; i++)
+    {
+        var edge = sortedEdges[i];
+        if(!(nodes.includes(edge.source) && nodes.includes(edge.target)))
+        {
+            newEdges.push(edge);
+            if(!nodes.includes(edge.source))
+            {
+                nodes.push(edge.source);
+            }
+            if(!nodes.includes(edge.target))
+            {
+                nodes.push(edge.target);
+            }
+        }
+    }
+
+    return newEdges;
+}
+
+
+function primsAlgorithm(startPoint, nodeEdgeMap)
+{
+    var nextNode = startPoint;
+    var processedNodes = [nextNode];
+    var edgeQueue = new buckets.PriorityQueue(function(a,b)
+    {
+        return -a.weight + b.weight;
+    });
+    for(i = 0; i < this.allNeighborsCount[nextNode] + 1; i++)
+    {
+        if(i == nextNode)
+        {
+            continue;
+        }
+        edgeQueue.enqueue(this.allNeighborsIndex[nextNode][i][nodeEdgeMap.get({a: nextNode, b: i})]);
+    }
+    var newEdges = new Array();
+
+    while(!edgeQueue.isEmpty())
+    {
+        var edge = edgeQueue.dequeue();
+        var prevNode = nextNode;
+        nextNode = edge.source == nextNode ? edge.target : edge.source;
+        if(!processedNodes.includes(nextNode))
+        {
+            processedNodes.push(nextNode);
+            newEdges.push(edge);
+            for(i = 0; i < this.allNeighborsCount[nextNode] + 1; i++)
+            {
+                if(i == nextNode)
+                {
+                    continue;
+                }
+                edgeQueue.enqueue(this.allNeighborsIndex[nextNode][i][nodeEdgeMap.get({a: nextNode, b: i})]);
+            }
+        }        
+    }
+    this.clear();
+    this.read({nodes: nodeArray, edges: newEdges});
 }
 
 buckets.Queue.prototype.maxSize = 0;
